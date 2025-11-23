aa60f96603b6360b3fffa2ca66730cf740139473
Snehal Sandip Dube <snehaldube36@gmail.com>
2025-11-23 16:46:13 +0530

    Add comment for descending order condition in InsertionSort
    Added a comment to indicate the condition for descending order sorting.

--- InsertionSort.java
+++ InsertionSort.java
@@ -18,7 +18,7 @@ public class InsertionSort
         {
             int key = A[i];
             int j = i-1;
-            while(j>=0 && A[j]>key)
+            while(j>=0 && A[j]>key)            // For Descending order condition -> while(j>=0 && A[j]<key)
             {
                 A[j+1] = A[j];
                 j--;
@@ -33,3 +33,4 @@ public class InsertionSort
         }
     }
 }
+
